1d68a07b494b66bf6cd44b2f4bf3ede2b648b2ec
lenovo <lenovo@LAPTOP-BLNTEBH7>
2022-07-27 21:55:37 +0530

    Commit the code for controller and project strture

---
+++ PremiumCalculator.WebAPI/Controllers/PremiumCalculatorController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PremiumCalculator.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PremiumCalculatorController : ControllerBase
+    {
+    }
+}
