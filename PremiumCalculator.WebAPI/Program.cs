954597e5958c84ca709067d9fc50d0fdafe46e9d
lenovo <lenovo@LAPTOP-BLNTEBH7>
2022-07-28 09:55:26 +0530

    changes for cors enable

--- PremiumCalculator.WebAPI/Program.cs
+++ PremiumCalculator.WebAPI/Program.cs
@@ -31,6 +31,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(builder =>
+{
+    builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+});
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
